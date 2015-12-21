a0c3896d52b1ee9d652db0c4ad244e00d9ccd1a0
bdepwgjqet <bdepwgjqet@gmail.com>
2015-12-22 07:06:24 +0800

    add douban movie

---
+++ douban_movie/douban_movie/spiders/dbmv_spider.py
@@ -0,0 +1,64 @@
+import scrapy
+import json
+import re
+from scrapy.selector import Selector
+
+class DbmvSpider(scrapy.Spider):
+    name = "dbmv"
+    allowed_domains = ["movie.douban.com"]
+    start_urls = [
+        "http://movie.douban.com/tag/"
+    ]
+
+    def parse(self, response):
+        url_list = response.css('a[href^="http"]').xpath('@href').extract()
+        url_list = [x for x in url_list if re.match("http://www.douban.com/tag/\d{4}/\?focus=movie$",x)]
+        yield scrapy.Request(url_list[0], callback=self.parse_tag, dont_filter=True)
+        '''
+        for url in url_list:
+            print url
+            yield scrapy.Request(url, callback=self.parse_tag, dont_filter=True)
+            '''
+
+    def parse_tag(self, response):
+        url_list = response.css('a.more-links').xpath('@href').extract()
+        url_list = [x for x in url_list if re.match("^http://www.douban.com/link2/.*?mod=movie$",x)]
+        yield scrapy.Request(url_list[0], callback=self.parse_movie_list, dont_filter=True)
+
+'''
+        for url in url_list:
+            print url
+            yield scrapy.Request(url, callback=self.parse_movie_list, dont_filter=True)
+            '''
+
+    def parse_movie_list(self, response):
+        url_list = response.css('a[href^="http"]').xpath('@href').extract()
+        url_list = [x for x in url_list if re.match("http://movie.douban.com/subject/\d+",x)]
+
+        yield scrapy.Request(url_list[0], callback=self.parse_movie_detail, dont_filter=True)
+        '''
+        for url in url_list:
+            yield scrapy.Request(url, callback=self.parse_movie_detail, dont_filter=True)
+            '''
+
+        next_url = response.css('span.next a').xpath('@href').extract()
+        if len(next_url) == 1:
+            nurl = re.sub('\?start=\d+','',response.url) + next_url[0]
+            print nurl
+            c = re.match('\?start=(\d+)',next_url[0])
+            if int(c.group(1)) <= 30:
+                yield scrapy.Request(nurl, callback=self.parse_movie_list, dont_filter=True)
+
+    def parse_movie_detail(self, response):
+        rt_json = {
+            "url": response.url,
+            "title": response.css('title::text').extract(),
+            "cn_title": response.css('[property="v:itemreviewed"]::text').extract()
+        }
+
+        filename = response.url.split("/")[-2] + '.json'
+        #filename = 'detail.html'
+        output_folder = "output/"
+        with open(output_folder + filename, 'wb') as f:
+            f.write(json.dumps(rt_json))
+            #f.write(response.body)
